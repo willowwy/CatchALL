7a268639555d46a500e1941875c67ca2745b7b4b
zjq <793746917@qq.com>
2022-06-21 00:49:27 +0800

    1

--- start.py
+++ start.py
@@ -22,17 +22,17 @@ if __name__ == '__main__':
     transform_file = './program_file/xml_data'
     srcml_path = 'D:\software\srcML\srcml.exe' 
     # 1.将 for.c 使用 scrML变化为for.xml
-    pre_path = os.path.join(program_path, 'for_text.c')
-    xml_path = os.path.join(transform_file, 'for.xml')
+    pre_path = os.path.join(program_path, 'test.c')
+    xml_path = os.path.join(transform_file, 'test.xml')
     # 1.将 for.c 使用 scrML变化为for.xml
     srcml_program_xml(pre_path, xml_path)
 
-    input_path = os.path.join(transform_file, 'for.xml')
-    output_path = os.path.join(transform_file, 'while.xml')
+    # input_path = os.path.join(transform_file, 'for.xml')
+    # output_path = os.path.join(transform_file, 'while.xml')
 
-    #  2.将for.xml通过规则变换，变化为while.xml
-    for_while.program_transform(input_path, output_path)
-    #   3. 将while.xml通过scrML还原为while.c
-    to_xml_path = os.path.join(transform_file, 'while.xml')
-    to_pre_path = os.path.join(program_path, 'while_trans.c')
-    srcml_xml_program(to_xml_path, to_pre_path)
+    # #  2.将for.xml通过规则变换，变化为while.xml
+    # for_while.program_transform(input_path, output_path)
+    # #   3. 将while.xml通过scrML还原为while.c
+    # to_xml_path = os.path.join(transform_file, 'while.xml')
+    # to_pre_path = os.path.join(program_path, 'while_trans.c')
+    # srcml_xml_program(to_xml_path, to_pre_path)
